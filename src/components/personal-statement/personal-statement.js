56fad4e3cd1f594e81891e36540f4c194afe69b6
Syed Ali <ali.syed.w2@gmail.com>
2020-12-15 21:11:06 +0000

    Add link to contact me text in personal statement

--- src/components/personal-statement/personal-statement.js
+++ src/components/personal-statement/personal-statement.js
@@ -8,7 +8,13 @@ const PersonalStatement = () => {
         Hi! I'm Syed, a passionate programmer and problem-solver.
         I love formulating creative solutions to real-world problems and pride
         myself on the quality of my work. If you would like to discuss any new opportunities
-        shoot me an email by clicking 'Contact Me' above.
+        shoot me an email by clicking '<a
+              href="mailto:ali.syed.w2@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact Me
+            </a>' above.
       </p>
     </div>
   )
