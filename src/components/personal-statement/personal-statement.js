0a8c94253d5dfdf21b610f05a1bc42f1d0965177
Syed Ali <ali.syed.w2@gmail.com>
2020-12-15 15:16:52 +0000

    Changes to about me section

--- src/components/personal-statement/personal-statement.js
+++ src/components/personal-statement/personal-statement.js
@@ -5,7 +5,7 @@ const PersonalStatement = () => {
   return (
     <div>
       <p className={personalStatementStyles.statement}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
+        Loremm Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
