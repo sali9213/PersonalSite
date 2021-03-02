af0202237f0f74cfa383ce219673d3d98d18d438
Syed Ali <ali.syed.w2@gmail.com>
2021-03-02 16:13:50 +0000

    test commit

--- src/components/personal-statement/personal-statement.js
+++ src/components/personal-statement/personal-statement.js
@@ -5,7 +5,7 @@ const PersonalStatement = () => {
   return (
     <div>
       <p className={personalStatementStyles.statement}>
-        Hi! I'm Syed, a passionate programmer and problem-solver.
+        Hi! I'm Syed ss, a passionate programmer and problem-solver.
         I love formulating creative solutions to real-world problems and pride
         myself on the quality of my work. If you would like to discuss any new opportunities
         shoot me an email by clicking '<a
